943c76292f6105df759f2b347de0b7abd30d0359
tkgnorth <gnorthsnipe@gmail.com>
2018-06-10 21:47:10 +0900

    Lesson6 StepAsideUnityChan_課題
    課題及び発展課題の内容に従いスクリプトを書きました。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+
+		this.unitychan = GameObject.Find("unitychan");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.transform.position.z < unitychan.transform.position.z - 10) {
+			Destroy (this.gameObject);
+		}
+
+	}
+}
